2061112037af9708b540fd75fd901cb2aeb376d5
Aaron Vigal <admin@aaronvigal.com>
2016-07-23 18:07:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -105,6 +105,7 @@ Radiobutton(app, text="Morse", value="Morse", variable=relStatus, command=beenCl
 Radiobutton(app, text="Auto", value="Auto", variable=relStatus, command=beenClicked).grid(row=2, column=4)
 text = Text(app)
 text.insert(INSERT, "---------------------------------------------------------")
+text.config(state=DISABLED)
 text.grid(row=3,columnspan=4, height=1)
 # Start the application
 app.mainloop()
