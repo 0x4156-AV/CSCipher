bd3fbd472785d9f4d47b7f6b01b126e3ee662414
Aaron Vigal <admin@aaronvigal.com>
2016-07-23 13:10:32 -0500

    Update caesar.py

--- modules/caesar.py
+++ modules/caesar.py
@@ -1,1 +1,36 @@
-def caear():
+import collections
+from string import ascii_lowercase
+
+def find_key():
+    number = len(string)
+    array = []
+
+    dict = collections.defaultdict(int)
+    for character in string:
+        dict[character] += 1
+
+    for character in ascii_lowercase:
+    	array.append((dict[character]/float(number)))
+
+    num = array.index(max(array))
+    key = (((num - 5) % 26) + 1)
+    return key
+
+def decrypt(n, ciphertext):
+    key = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    result = ''
+
+    for l in ciphertext:
+        try:
+            i = (key.index(l) - n) % 26
+            result += key[i]
+        except ValueError:
+            result += l
+
+    return result
+
+def caesar(string):
+	key = str(find_key())
+	decrypted = str(caesar_decrypt(find_key(), string.lower()))
+	print "Used key: " + key + "\n" + decrypted
+	
