ed8803d36993cfdfde152ea14459706b4228fca9
Aaron Vigal <admin@aaronvigal.com>
2016-07-23 14:58:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def beenClicked():
 labelText = StringVar(None)
 textEntry = Entry(app, width=57, justify=CENTER, textvariable=labelText).grid(row=1, column=1)
 submitButton = Button(app, text="Start", width=10, command=callback).grid(row=1, column=2)
-relStatus = StringVar().set()
+relStatus = StringVar()
 radiobutton = Radiobutton(app, text="Caesar", value="Caesar", variable=relStatus, command=beenClicked, width=5).grid(row=2, column=1)
 radiobutton = Radiobutton(app, text="Vigenere", value="Vigenere", variable=relStatus, command=beenClicked).grid(row=2, column=2)
 radiobutton = Radiobutton(app, text="Baconian", value="Baconian", variable=relStatus, command=beenClicked).grid(row=2, column=3)
