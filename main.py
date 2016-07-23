be1b149b998128a25921238049fbf501d8770afe
Aaron Vigal <admin@aaronvigal.com>
2016-07-23 14:14:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def callback():
     caesar(string)
 
 # Make and put the buttons on a grid on the window
-textEntry = Entry(app, width=57, justify=CENTER).set(none).grid(row=1, column=1)
+custName = StringVar().set(None)
+textEntry = Entry(app, width=57, justify=CENTER, textvariable=custname).grid(row=1, column=1)
 submitButton = Button(app, text="Start", width=10, command=callback).grid(row=1, column=2)
 
 # Start the application
