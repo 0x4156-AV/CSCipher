da164cf877aad5d08f17366b0d844ec753cefcda
Aaron Vigal <admin@aaronvigal.com>
2016-07-23 14:49:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,18 @@ def callback():
     string = labelText.get()
     tkMessageBox.showinfo("Caesar Cipher finished",caesar(string))
 
+def beenClicked():
+    
+
 # Make and put the buttons on a grid on the window
 labelText = StringVar(None)
 textEntry = Entry(app, width=57, justify=CENTER, textvariable=labelText).grid(row=1, column=1)
 submitButton = Button(app, text="Start", width=10, command=callback).grid(row=1, column=2)
+relStatus = StringVar().set(None)
+radiobutton = RadioButton(app, text="", value="", variable=relStatus, command=beenClicked).grid(row=2, column=1)
+radiobutton = RadioButton(app, text="", value="", variable=relStatus, command=beenClicked).grid(row=2, column=1)
+radiobutton = RadioButton(app, text="", value="", variable=relStatus, command=beenClicked).grid(row=2, column=1)
+radiobutton = RadioButton(app, text="", value="", variable=relStatus, command=beenClicked).grid(row=2, column=1)
 
 # Start the application
 app.mainloop()
