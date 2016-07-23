d18c74eaf11e267d24b11e1eb5993223051680d7
Aaron Vigal <admin@aaronvigal.com>
2016-07-23 13:41:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import sys, os
+from tkinter import *
+import tkinter.messagebox
 from caesar import caesar
-flagFormat = input("How are flags formatted for this CTF? (ex. flag{}) ")
-string="wcrx{alczlj_trvjri}"
-caesar(string)
+
+app = TK()
+ap.title("CSCipher")
+app.geometry("450x300+200+200")
+app.mainloop()
+
+#flagFormat = input("How are flags formatted for this CTF? (ex. flag{}) ")
+#string="wcrx{alczlj_trvjri}"
+#caesar(string)
