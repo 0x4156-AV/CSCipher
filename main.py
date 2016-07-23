206d9fe2f4e7841e656256b96deed51b064b4946
Aaron Vigal <admin@aaronvigal.com>
2016-07-23 14:17:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def callback():
 
 # Make and put the buttons on a grid on the window
 labelText = StringVar().set("")
-textEntry = Entry(app, width=57, justify=CENTER, textvariable=lableText).grid(row=1, column=1)
+textEntry = Entry(app, width=57, justify=CENTER, textvariable=labelText).grid(row=1, column=1)
 submitButton = Button(app, text="Start", width=10, command=callback).grid(row=1, column=2)
 
 # Start the application
