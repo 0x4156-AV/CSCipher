1c9de952eab46cdec7d5ddce586a166c5c530e6f
Aaron Vigal <admin@aaronvigal.com>
2016-07-23 15:58:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,8 @@ textEntry = Entry(app, width=57, justify=CENTER, textvariable=labelText).grid(ro
 submitButton = Button(app, text="Start", width=10, command=callback).grid(row=1, column=4, pady=5)
 
 # Then put down the options for what cipher to use
-relStatus = StringVar().set("Caesar")
+relStatus = StringVar()
+relStatus.set("Caesar")
 radiobutton1 = Radiobutton(app, text="Caesar", value="Caesar", variable=relStatus, command=beenClicked).grid(row=2, column=0)
 radiobutton2 = Radiobutton(app, text="Vigenere", value="Vigenere", variable=relStatus, command=beenClicked).grid(row=2, column=1)
 radiobutton3 = Radiobutton(app, text="Baconian", value="Baconian", variable=relStatus, command=beenClicked).grid(row=2, column=2)
