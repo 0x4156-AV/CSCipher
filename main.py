4ab6f57b1585531ab98c9b6ff37def354f83881f
Aaron Vigal <admin@aaronvigal.com>
2016-10-12 02:37:45 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+# Import all my crap
 from checkflag import *
 from ciphers.bases import *
 from ciphers.caesar import *
 from ciphers.morse import *
 from ciphers.baconain import *
-message = "AAAAAAAAABAAABAAAABBAABAAAABABAABBAAABBBABAAAABAABABABAABABBABBAAABBABABBBAABBBBBAAAABAAABBAABABAABBBABAABABABBABBABABBBBBAAABBAAB"
+from ciphers.vigenere import *
+message = ""
+# Setup all Classes
 flagFormat = checkFlag()
 caesar = Caesar()
 bases = Bases()
 morse = Morse()
 baconain = Baconain()
+vigenere = Vigenere()
+# Run decryption functions
 caesar.caesar(message)
 flagFormat.check_array(caesar.get_results())
 flagFormat.check(bases.base16(message))
@@ -16,3 +21,5 @@ flagFormat.check(bases.base32(message))
 flagFormat.check(bases.base64(message))
 flagFormat.check(morse.morse(message))
 flagFormat.check(baconain.baconain(message))
+flagFormat.check(vigenere.vigenere(message))
+# Viaola!
