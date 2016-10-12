bfae332c0b3b14dfcbcbd484bef2c4a0d8df9219
Aaron Vigal <admin@aaronvigal.com>
2016-10-12 01:35:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from checkflag import *
 from ciphers.bases import *
 from ciphers.caesar import Caesar
-message = "MFRGG"
+message = "MZWGCZ33NBUX2"
 flagFormat = checkFlag()
 caesar = Caesar()
 bases = Bases()
-flagFormat.check(caesar.caesar(message))
+caesar.caesar(message)
+flagFormat.check_array(caesar.get_results())
 flagFormat.check(bases.base16(message))
 flagFormat.check(bases.base32(message))
 flagFormat.check(bases.base64(message))
