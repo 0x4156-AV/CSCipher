c53a858eede60f2e73d0d5f73c401856b0d8638d
Aaron Vigal <admin@aaronvigal.com>
2016-10-12 01:01:21 -0500

    Update checkflag.py

--- checkflag.py
+++ checkflag.py
@@ -1,8 +1,16 @@
-class checkFlag(object):
-    def __init__(slef):
+import sys
+class checkFlag():
+    def __init__(self):
         ask_format = input("What is the format of your flags? ").upper()
         accepted = "ABCDEFGHIJKLMNOPQRSTVWXYZ"
         flag_format = ""
         for i in ask_format:
             if i in accepted:
                 flag_format += i
+        flag_format += "{"
+        print(flag_format)
+
+    def check(string):
+        if(string.contains(self.flag_format)):
+            print("Flag: " + string)
+            sys.exit(0)
